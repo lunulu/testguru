f66c4f9ff38086c3b970e024bf8d61495350b63a
lunu <fairus200599@gmail.com>
2022-09-03 16:08:43 +0300

    feat: part 6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,19 @@
 class Answer < ApplicationRecord
+  NUMBER_OF_PERMITTED_ANSWERS = 4
+
   belongs_to :question
+
+  scope :correct, -> { where(correct: true) }
+
+  validates :body, presence: true
+
+  validate :questions_answers_limit
+
+  private
+
+  def questions_answers_limit
+    if question_id && question.answers.count >= NUMBER_OF_PERMITTED_ANSWERS
+      errors.add(:question_id, 'Answer cannot be added to that question')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
 
   has_many :created_tests, class_name: 'Test'
 
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   def started_tests_by_level(test_level)
     tests.where(level: test_level)
   end
