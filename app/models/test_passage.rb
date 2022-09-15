cf7fd21c18725b6b0d93d0e557eb18f7e3bb9cef
lunu <fairus200599@gmail.com>
2022-09-15 18:07:25 +0300

    feat: part 9 tasks

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,6 +5,8 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
+  before_update :before_save_set_next_question
+
   def completed?
     current_question.nil?
   end
@@ -12,7 +14,6 @@ class TestPassage < ApplicationRecord
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
 
-    self.current_question = next_question
     save!
   end
 
@@ -22,18 +23,17 @@ class TestPassage < ApplicationRecord
     self.current_question = test.questions.first if test.present?
   end
 
-  def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
+  def before_save_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  end
 
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-      correct_answers_count == answer_ids.count
+  def correct_answer?(answer_ids)
+    answer_ids = [] if answer_ids.nil?
+
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
