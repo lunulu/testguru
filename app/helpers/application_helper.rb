0e4c825b1df0845f67b8edd6c1ed77e9d9024780
lunu <fairus200599@gmail.com>
2022-09-18 20:37:11 +0300

    feat: part 10 fixes v2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'Test Guru', "https://github.com/#{author}/#{repo}"
   end
+
+  def flashes
+    return unless flash.any?
+
+    flash.map do |type, message|
+      content_tag :p, message, class: "flash #{type}"
+    end.join.html_safe
+  end
 end
