97be399bb2a36e1ad91a6da86b0c248917d032b2
lunu <fairus200599@gmail.com>
2022-09-20 20:05:23 +0300

    feat: part 11

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,61 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :set_test, only: %i[show edit update destroy start]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def edit; end
+
+  def create
+    @test = Test.new(test_params)
+    @test.author = current_user
+    if @test.save
+      redirect_to [:admin, @test]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to [:admin, @test]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :user_id)
+  end
+
+  def rescue_with_test_not_found
+    render inline: '<p>Test #<%= params[:id] %> not found</p>'
+  end
+end
