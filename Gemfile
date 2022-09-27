a11222384e75845eeefad213b0e90fe7c937fcab
lunu <fairus200599@gmail.com>
2022-09-27 16:29:47 +0300

    Part 13 (#13)
    feat: part 13

--- Gemfile
+++ Gemfile
@@ -35,6 +35,11 @@ gem 'devise'
 
 gem 'rails-i18n'
 
+# UI
+
+gem 'jquery-rails'
+gem 'bootstrap', '~> 5.2.1'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,9 +1,9 @@
 module TestPassagesHelper
   def result_message(test_passage)
     if test_passage.success?
-      "<p class='green-text'>#{I18n.t('.test_passages_helper.success')}</p>".html_safe
+      "<p class='text-success'>#{I18n.t('.test_passages_helper.success')}</p>".html_safe
     else
-      "<p class='red-text'>#{I18n.t('.test_passages_helper.fail')}</p>".html_safe
+      "<p class='text-danger'>#{I18n.t('.test_passages_helper.fail')}</p>".html_safe
     end
   end
 
