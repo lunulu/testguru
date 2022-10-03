ed2b8e6d61a6f5527cff5f024d16869d694a14cc
lunu <fairus200599@gmail.com>
2022-10-03 22:33:25 +0300

    feat: part 15 fix

--- app/assets/javascripts/utilities/password_confirmation.js
+++ app/assets/javascripts/utilities/password_confirmation.js
@@ -1,24 +1,27 @@
 document.addEventListener('turbolinks:load', function() {
 
-	let input1 = document.getElementById('user_password')
-	let input2 = document.getElementById('user_password_confirmation')
+    let input1 = document.getElementById('user_password')
+    let input2 = document.getElementById('user_password_confirmation')
 
-	if (input1) { input1.addEventListener('keyup', checkPasswords) }
-	if (input2) { input2.addEventListener('keyup', checkPasswords) }
+    if (input1) { input1.addEventListener('keyup', checkPasswords) }
+    if (input2) { input2.addEventListener('keyup', checkPasswords) }
 })
 
 function checkPasswords () {
+    let password = document.getElementById('user_password')
+    let password_confirmation = document.getElementById('user_password_confirmation')
 
-	let password = document.getElementById('user_password')
-	let password_confirmation = document.getElementById('user_password_confirmation')
+    if (!password_confirmation.value) {
+        password_confirmation.classList.remove('bg-success')
+        password_confirmation.classList.remove('bg-danger')
+        return
+    }
 
-	if (password_confirmation.value) {
-		if (password.value === password_confirmation.value) {
-			password_confirmation.classList.remove('bg-danger')
-			password_confirmation.classList.add('bg-success')
-		} else {
-			password_confirmation.classList.remove('bg-success')
-			password_confirmation.classList.add('bg-danger')
-		}
-	}
+    if (password.value === password_confirmation.value) {
+        password_confirmation.classList.remove('bg-danger')
+        password_confirmation.classList.add('bg-success')
+    } else {
+        password_confirmation.classList.remove('bg-success')
+        password_confirmation.classList.add('bg-danger')
+    }
 }
