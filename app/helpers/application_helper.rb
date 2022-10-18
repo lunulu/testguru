a79c8cb0985a8b8f084d58a6992d24b689c88f0a
lunu <fairus200599@gmail.com>
2022-10-18 18:50:05 +0300

    feat: part 17 lesson

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to 'Test Guru', "https://github.com/#{author}/#{repo}"
+    link_to 'GitHub', "https://github.com/#{author}/#{repo}", class: 'nav-link px-2 text-muted'
   end
 
   def current_translations
