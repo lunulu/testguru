4f5f25e0bb205b02fdf286046f2c7a50bd1cc0c6
lunu <fairus200599@gmail.com>
2022-10-20 13:49:13 +0300

    feat: part17 update action_mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testguru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'lunu-testguru.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
