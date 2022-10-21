cd35d1707dbca3f22674a051f96edb94ad3a73ef
lunu <fairus200599@gmail.com>
2022-10-21 19:26:49 +0300

    feat: full localization

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
   def question_header(question)
     if question.new_record?
-      "Create new #{question.test.title} Question"
+      I18n.t('questions_helper.question_header.create', test_title: question.test.title)
     else
-      "Edit #{question.test.title} Question"
+      I18n.t('questions_helper.question_header.edit', test_title: question.test.title)
     end
   end
 end
