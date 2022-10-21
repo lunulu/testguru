ec28cbb106ee7cb3865123e6d785afc1b952bee2
lunu <fairus200599@gmail.com>
2022-10-21 16:14:10 +0300

    fix: add null:false for correct in questions table

---
+++ db/migrate/20221021125551_add_null_false_for_correct_to_answers.rb
@@ -0,0 +1,5 @@
+class AddNullFalseForCorrectToAnswers < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :answers, :correct, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_28_191339) do
+ActiveRecord::Schema.define(version: 2022_10_21_125551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
     t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
